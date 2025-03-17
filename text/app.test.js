4b1d4dadbd69a748e468fda6257eec717da67309
Iverson Inoel Rosario De Jesus <iversoninoel@gmail.com>
2025-03-17 10:46:33 -0400

    .

---
+++ text/app.test.js
@@ -0,0 +1,7 @@
+const request = require("supertest");
+const app = require("../src/index");
+
+test("Debe responder con un mensaje de bienvenida", async () => {
+    const response = await request(app).get("/");
+    expect(response.text).toBe("Hello, CI/CD Pipeline!");
+});
